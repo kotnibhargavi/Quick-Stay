0f133a8668998352b05204648c2abce209c07f90
kotnibhargavi <kotnibhargavi0103@gmail.com>
2025-06-12 10:21:18 +0530

    contextChanging

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,8 +11,6 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PUBLISHABLE_KEY) {
     throw new Error('Add your Clerk Publishable Key to the .env file')
   }
-  
-
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
    <BrowserRouter>
