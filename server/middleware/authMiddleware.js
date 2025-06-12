a5b7fcc1e680e1f4ee90ef14e017c25b429a0620
kotnibhargavi <kotnibhargavi0103@gmail.com>
2025-06-12 13:36:24 +0530

    Middleware changes in backend

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,12 +4,12 @@ import User from "../models/User.js";
 export const ProtectedRoute = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
-    if (!token) return res.json({ success: false, message: "No token provided" });
+    if (!token) return res.json({ success: false, message: "Unauthorized" });
 
     const decoded = jwt.decode(token);
     const userId = decoded?.sub;
 
-    if (!userId) return res.json({ success: false, message: "Invalid token" });
+    if (!userId) return res.json({ success: false, message: "User ID not found" });
 
     const user = await User.findById(userId);
     if (!user) return res.json({ success: false, message: "User not found" });
